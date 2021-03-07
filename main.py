3a9a5436ee5f5e14ee9d8223518af6c8b06c7c33
Plextora Lunar <plextoraa.a@gmail.com>
2021-03-07 20:27:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+secret_number = 5
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+  guess = int(input("Guess a number and get lucky: "))
+  guess_count += 1 
+  if guess_count == 1:
+    print("You got it wrong, you have 2 more tries left.")
+  if guess_count == 2:
+    print("You got it wrong again, this is your last try, so make it count!")
+    if guess == secret_number:
+        print("You won!")
+        break
+else:
+  print("Sorry, you didn't get lucky :(")
