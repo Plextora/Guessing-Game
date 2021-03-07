654fe50b814c15aeccf28bbc7c1e52261696b4de
Plextora Lunar <plextoraa.a@gmail.com>
2021-03-07 20:43:32 +0000

    Edited a bit of the wrong answer text

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ while guess_count < guess_limit:
   guess = int(input("Guess a number and get lucky: "))
   guess_count += 1 
   if guess_count == 1:
+    if guess == secret_number:
+        print("You won!")
+        break
     print("You got it wrong, you have 2 more tries left.")
   if guess_count == 2:
-    print("You got it wrong again, this is your last try, so make it count!")
     if guess == secret_number:
+        print("You won!")
+        break
+    print("You got it wrong again, this is your last try, so make it count!")
+  if guess == secret_number:
         print("You won!")
         break
 else:
